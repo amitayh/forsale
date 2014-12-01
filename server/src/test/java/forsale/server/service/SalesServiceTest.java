e90acedd06aa38ce6a6a6e87d7520edc29de3d7f
Amitay Horwitz <amitayh@il.ibm.com>
2014-12-01 19:00:13 +0200

    Sales service tests

--- server/src/test/java/forsale/server/service/SalesServiceTest.java
+++ server/src/test/java/forsale/server/service/SalesServiceTest.java
@@ -14,6 +14,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class SalesServiceTest {
 
@@ -21,26 +23,44 @@ public class SalesServiceTest {
 
     public static final int TEST_DB_REDIS = 1;
 
-    private Container container;
+    private VendorsServiceInterface vendors;
+
+    private SalesServiceInterface sales;
 
     @Before
     public void setUp() throws Exception {
-        container = getTestContainer();
+        Container container = getTestContainer();
         flush((Connection) container.get("mysql"));
         flush((Jedis) container.get("redis"));
+
+        vendors = (VendorsServiceInterface) container.get("service.vendors");
+        sales = (SalesServiceInterface) container.get("service.sales");
     }
 
     @After
     public void tearDown() {
-        container = null;
+        vendors = null;
+        sales = null;
     }
 
     @Test
     public void testInsertSale() throws Exception {
-        Container container = getTestContainer();
-        VendorsServiceInterface vendors = (VendorsServiceInterface) container.get("service.vendors");
-        SalesServiceInterface sales = (SalesServiceInterface) container.get("service.sales");
+        Vendor vendor = new Vendor();
+        vendor.setName("Vendor #1");
+        vendors.insert(vendor);
 
+        Sale sale = new Sale();
+        sale.setTitle("Sale #1");
+        sale.setVendor(vendor);
+        int saleId = sales.insert(sale);
+
+        assertEquals(saleId, sale.getId());
+        assertNotNull(saleId);
+        assertTrue(saleId > 0);
+    }
+
+    @Test
+    public void testGetPopularReturnsSalesSortedByPopularity() throws Exception {
         Vendor vendor = new Vendor();
         vendor.setName("Vendor #1");
         vendors.insert(vendor);
@@ -51,39 +71,28 @@ public class SalesServiceTest {
         sales.insert(sale1);
 
         Sale sale2 = new Sale();
-        sale2.setTitle("Sale #2");
+        sale2.setTitle("Sale #1");
         sale2.setVendor(vendor);
         sales.insert(sale2);
 
         Sale sale3 = new Sale();
-        sale3.setTitle("Sale #3");
+        sale3.setTitle("Sale #1");
         sale3.setVendor(vendor);
         sales.insert(sale3);
-
-        assertEquals(1, sale1.getId());
-        assertEquals(2, sale2.getId());
-        assertEquals(3, sale3.getId());
-    }
-
-    @Test
-    public void testGetPopularReturnsSalesSortedByPopularity() {
-        // insert sales
-
-        // insert views
-
-        // get popular
     }
 
     private Container getTestContainer() throws Exception {
         Container container = Bootstrap.createDependencyInjectionContainer();
         container.set("mysql.db", TEST_DB_MYSQL);
         container.set("redis.db", TEST_DB_REDIS);
+
         return container;
     }
 
     private void flush(Connection conn) throws SQLException {
         Statement stmt = conn.createStatement();
-        stmt.execute("TRUNCATE TABLE sales;");
+        stmt.execute("DELETE FROM sales;");
+        stmt.execute("DELETE FROM vendors;");
     }
 
     private void flush(Jedis redis) {
