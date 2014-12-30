d66f29eb23bb139fbbd27edb7bbe6a703005db31
Amitay Horwitz <amitayh@il.ibm.com>
2014-12-30 14:30:07 +0200

    BirthDate tests fix (ignore timezone)

--- server/src/test/java/forsale/server/domain/BirthDateTest.java
+++ server/src/test/java/forsale/server/domain/BirthDateTest.java
@@ -3,20 +3,26 @@ package forsale.server.domain;
 import forsale.server.domain.exception.ValidationException;
 import org.junit.Test;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 import static org.junit.Assert.assertEquals;
 
 public class BirthDateTest {
 
     @Test
     public void testGetTime() throws Exception {
-        BirthDate birthDate = new BirthDate("2014-12-01");
-        assertEquals(1417384800000L, birthDate.getTime());
+        String dateString = "2014-12-01";
+        BirthDate birthDate = new BirthDate(dateString);
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        assertEquals(dateString, dateFormat.format(birthDate.getTime()));
     }
 
     @Test
     public void testToString() throws Exception {
-        BirthDate birthDate = new BirthDate("2014-12-01");
-        assertEquals("2014-12-01", birthDate.toString());
+        String dateString = "2014-12-01";
+        BirthDate birthDate = new BirthDate(dateString);
+        assertEquals(dateString, birthDate.toString());
     }
 
     @Test(expected = ValidationException.class)
