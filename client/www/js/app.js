c4eeb4d12a1432abbdec48e01f413b7b6eb48ae5
Amitay Horwitz <amitayh@il.ibm.com>
2015-02-25 14:06:22 +0200

    Sales - recent and detail

--- client/www/js/app.js
+++ client/www/js/app.js
@@ -7,106 +7,120 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
+  })
+
+  .config(function ($stateProvider, $urlRouterProvider) {
+
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
+
+      // Setup an abstract state for the tabs directive
+
+      .state('tab', {
+        url: "/tab",
+        abstract: true,
+        templateUrl: "templates/tabs.html"
+      })
+
+      // Each tab has its own nav history stack:
+
+      .state('tab.recent', {
+        url: '/recent',
+        views: {
+          'tab-recent': {
+            templateUrl: 'templates/tab-recent.html',
+            controller: 'RecentCtrl'
+          }
+        }
+      })
+      .state('tab.sale', {
+        url: '/sale/:saleId',
+        views: {
+          'tab-recent': {
+            templateUrl: 'templates/sale-detail.html',
+            controller: 'SaleDetailCtrl'
+          }
+        }
+      })
+
+      ////////////////////////////////////////////////////////////////////////////////////////////
+      // TODO: REMOVE THESE
+      ////////////////////////////////////////////////////////////////////////////////////////////
+
+      .state('tab.dash', {
+        url: '/dash',
+        views: {
+          'tab-dash': {
+            templateUrl: 'templates/tab-dash.html',
+            controller: 'DashCtrl'
+          }
+        }
+      })
+
+      .state('tab.chats', {
+        url: '/chats',
+        views: {
+          'tab-chats': {
+            templateUrl: 'templates/tab-chats.html',
+            controller: 'ChatsCtrl'
+          }
+        }
+      })
+      .state('tab.chat-detail', {
+        url: '/chats/:chatId',
+        views: {
+          'tab-chats': {
+            templateUrl: 'templates/chat-detail.html',
+            controller: 'ChatDetailCtrl'
+          }
+        }
+      })
+
+      .state('tab.friends', {
+        url: '/friends',
+        views: {
+          'tab-friends': {
+            templateUrl: 'templates/tab-friends.html',
+            controller: 'FriendsCtrl'
+          }
+        }
+      })
+      .state('tab.friend-detail', {
+        url: '/friend/:friendId',
+        views: {
+          'tab-friends': {
+            templateUrl: 'templates/friend-detail.html',
+            controller: 'FriendDetailCtrl'
+          }
+        }
+      })
+
+      .state('tab.account', {
+        url: '/account',
+        views: {
+          'tab-account': {
+            templateUrl: 'templates/tab-account.html',
+            controller: 'AccountCtrl'
+          }
+        }
+      });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/dash');
+
   });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: "/tab",
-    abstract: true,
-    templateUrl: "templates/tabs.html"
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.recent', {
-    url: '/recent',
-    views: {
-      'tab-recent': {
-        templateUrl: 'templates/tab-recent.html',
-        controller: 'RecentCtrl'
-      }
-    }
-  })
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.friends', {
-      url: '/friends',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/tab-friends.html',
-          controller: 'FriendsCtrl'
-        }
-      }
-    })
-    .state('tab.friend-detail', {
-      url: '/friend/:friendId',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/friend-detail.html',
-          controller: 'FriendDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
-
-});
--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -1,37 +1,50 @@
 angular.module('starter.controllers', [])
 
-    .controller('RecentCtrl', function($scope, Sales) {
-        $scope.sales = [];
-        Sales.recent().then(function(sales) {
-            $scope.sales = sales;
-        }, function() {
-            $scope.sales = [];
-        });
-    })
+  .controller('RecentCtrl', function ($scope, Sales) {
+    $scope.sales = [];
+    Sales.recent().then(function (sales) {
+      $scope.sales = sales;
+    }, function () {
+      $scope.sales = [];
+    });
+  })
+  .controller('SaleDetailCtrl', function ($scope, $stateParams, Sales) {
+    $scope.sale = null;
+    Sales.detail($stateParams.saleId).then(function (sale) {
+      $scope.sale = sale;
+    }, function () {
+      $scope.sale = null;
+    });
+  })
 
-.controller('DashCtrl', function($scope) {})
+  ////////////////////////////////////////////////////////////////////////////////////////////
+  // TODO: REMOVE THESE
+  ////////////////////////////////////////////////////////////////////////////////////////////
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
-})
+  .controller('DashCtrl', function ($scope) {
+  })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+  .controller('ChatsCtrl', function ($scope, Chats) {
+    $scope.chats = Chats.all();
+    $scope.remove = function (chat) {
+      Chats.remove(chat);
+    }
+  })
 
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
+  .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
+  })
 
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
+  .controller('FriendsCtrl', function ($scope, Friends) {
+    $scope.friends = Friends.all();
+  })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+  .controller('FriendDetailCtrl', function ($scope, $stateParams, Friends) {
+    $scope.friend = Friends.get($stateParams.friendId);
+  })
+
+  .controller('AccountCtrl', function ($scope) {
+    $scope.settings = {
+      enableFriends: true
+    };
+  });
