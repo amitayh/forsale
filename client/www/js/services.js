afd0794ca565b3bb0eba26f63a364a713d5981da
Amitay Horwitz <amitayh@il.ibm.com>
2015-03-02 17:16:49 +0200

    Added users service

--- client/www/js/services.js
+++ client/www/js/services.js
@@ -101,6 +101,32 @@ angular.module('starter.services', [])
     };
   })
 
+  .factory('Users', function (API) {
+
+    function getFavorites() {
+      return API.get('/users/favorites');
+    }
+
+    function setFavorites(favorites) {
+      return API.post('/users/favorites', favorites);
+    }
+
+    function getProfile() {
+      return API.get('/users/profile');
+    }
+
+    function setProfile(user) {
+      return API.post('/users/profile', user);
+    }
+
+    return {
+      getFavorites: getFavorites,
+      setFavorites: setFavorites,
+      getProfile: getProfile,
+      setProfile: setProfile
+    };
+  })
+
   .factory('Sales', function (API) {
 
     function recent() {
